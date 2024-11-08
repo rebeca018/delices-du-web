b291635c16beb360ead06bca8b15faff9fd1bfc2
rebeca018 <rebecasnascimento018@gmail.com>
2024-11-08 15:38:32 -0400

    Finalizando o código

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
 let cacheName = "delices-du-web";
-let filesToCache = ["/", "/index.html", "/massas", "/doces", "/carnes", "/images","/manifest.json",
+let filesToCache = ["/", "/index.html", "/massas", "/doces", "/carnes", "/manifest.json",
                     "/CSS/styles.css", "/CSS/massas.css", "/CSS/doces.css", "/CSS/carnes.css", "/js/main.js",
-                    "/images/icon.png"];
+                    "/images/icon.png", "/images/image1.png", "/images/image2.png", "/images/image3.png", 
+                    "/images/image4.png", "/images/image5.png", "/images/image6.png",
+                    "/images/image7.png", "/images/image8.png", "/images/imagem9.png",
+                    "/images/pwa-icon-256.png", "/images/pwa-icon-512.png" ];
 
 /* inicializando a service worker e fazendo o download do conteúdo da aplicação */
 self.addEventListener("install", (e) => {
